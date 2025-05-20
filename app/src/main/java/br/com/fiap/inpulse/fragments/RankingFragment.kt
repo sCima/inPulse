ed0864c1c9c871039a31d07eb3ec900e140d891c
Lucas de Lima <lucasdelimabzr@gmail.com>
2025-05-20 15:08:30 -0300

    layout ranking

--- app/src/main/java/br/com/fiap/inpulse/fragments/RankingFragment.kt
+++ app/src/main/java/br/com/fiap/inpulse/fragments/RankingFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import br.com.fiap.inpulse.R
 
 class RankingFragment : Fragment() {
@@ -13,7 +15,17 @@ class RankingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_ranking, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_ranking, container, false)
 
+        val spinner: Spinner = view.findViewById(R.id.spinnerFiltro)
+        val adapter = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.filtros_array,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        return view
+    }
 }
