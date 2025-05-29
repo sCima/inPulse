0f728cbd685c58653c9f991d74211297c0c851a1
Lucas de Lima <lucasdelimabzr@gmail.com>
2025-05-29 13:35:36 -0300

    enviar sugestao como o problema na ideia

--- app/src/main/java/br/com/fiap/inpulse/fragments/idea/IdeaFragmentP.kt
+++ app/src/main/java/br/com/fiap/inpulse/fragments/idea/IdeaFragmentP.kt
@@ -1,10 +1,12 @@
 package br.com.fiap.inpulse.fragments.idea
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import br.com.fiap.inpulse.R
 
@@ -22,6 +24,24 @@ public class IdeaFragmentP : Fragment(), IdeaInfoProvider {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         etProblema = view.findViewById(R.id.et_problema_desc)
+
+        val sugestoesIds = listOf(
+            R.id.sugestao_problema1,
+            R.id.sugestao_problema2,
+            R.id.sugestao_problema3,
+            R.id.sugestao_problema4,
+            R.id.sugestao_problema5,
+            R.id.sugestao_problema6
+        )
+        val sugestoes = sugestoesIds.map { id -> view.findViewById<TextView>(id) }
+
+        sugestoes.forEach { tv ->
+            tv.setOnClickListener {
+                sugestoes.forEach { it.setTypeface(null, Typeface.NORMAL) }
+                tv.setTypeface(null, Typeface.BOLD)
+                etProblema.setText(tv.text)
+            }
+        }
     }
 
     override fun enviarDados(): Bundle {
