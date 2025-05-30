06cb5a09cfa6faa1c890a2d30634f2622578a253
Lucas de Lima <lucasdelimabzr@gmail.com>
2025-05-30 15:15:23 -0300

    activity login + cadastro

--- app/src/main/java/br/com/fiap/inpulse/CadastroActivity.kt
+++ app/src/main/java/br/com/fiap/inpulse/CadastroActivity.kt
@@ -1,6 +1,20 @@
 package br.com.fiap.inpulse
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class CadastroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+
+        val btnVoltar: ImageButton = findViewById(R.id.btn_voltar_cadastro)
+        btnVoltar.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
--- app/src/main/java/br/com/fiap/inpulse/LoginActivity.kt
+++ app/src/main/java/br/com/fiap/inpulse/LoginActivity.kt
@@ -2,6 +2,8 @@ package br.com.fiap.inpulse
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
@@ -12,24 +14,31 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         val etEmail = findViewById<EditText>(R.id.et_email_login)
-        val email = etEmail.text.toString().trim()
         val etSenha = findViewById<EditText>(R.id.et_senha_login)
-        val senha = etSenha.text.toString().trim()
         val btnEntrar: AppCompatButton = findViewById(R.id.btn_login)
+        val btnCadastro:  Button = findViewById(R.id.btn_cadastre)
 
         btnEntrar.setOnClickListener{
-//            if(email == "adm@email.com"){
-//                if(senha == "123"){
-//                    val intent = Intent(this, HubActivity::class.java)
-//                    startActivity(intent)
-//                } else {
-//                    etSenha.error = "Senha incorreta"
-//                    return@setOnClickListener
-//                }
-//            } else {
-//                etEmail.error = "Email incorreto"
-//                return@setOnClickListener
-//            }
+            val email = etEmail.text.toString().trim()
+            val senha = etSenha.text.toString().trim()
+
+            if(email == "adm@email.com"){
+                if(senha == "123"){
+                    val intent = Intent(this, HubActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    etSenha.error = "Senha incorreta"
+                    return@setOnClickListener
+                }
+            } else {
+                etEmail.error = "Email incorreto"
+                return@setOnClickListener
+            }
+        }
+
+        btnCadastro.setOnClickListener {
+            val intent = Intent(this, CadastroActivity::class.java)
+            startActivity(intent)
         }
 
     }
